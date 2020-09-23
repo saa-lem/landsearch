5abef6018e387576b099523c42aa50d12527e697
Salem Owino <salemowino18@gmail.com>
2020-09-23 16:21:00 +0300

    postitional argumentt corrected

--- rest/urls.py
+++ rest/urls.py
@@ -8,8 +8,10 @@ from . import views
 urlpatterns = [
   path('api/search/',UserPropertyListView.as_view(), name = 'user-projects'),
   path('api/properties/', views.PropertyView.as_view()),
-  path('api/properties/<int:pk>', views.PropertyView.as_view('put','delete')),
+  path('api/properties/<int:pk>', views.PropertyView.as_view('put')),
   path('api/properties/create', views.PropertyView.as_view('post')) 
+  path('api/properties/<int:pk>', views.PropertyView.as_view('delete')),
+  
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
