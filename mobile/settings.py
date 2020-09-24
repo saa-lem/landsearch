6b128a01719a900c11c0b448c57b7686e525ef55
Salem Owino <salemowino18@gmail.com>
2020-09-24 09:33:52 +0300

    congif added

--- mobile/settings.py
+++ mobile/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-from decouple import config,Csv
+from python_decouple import config,Csv
 import dj_database_url
 from pathlib import Path
 import django_heroku
