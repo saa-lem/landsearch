b7a142a6ea343c4f01e4d27f2b73551a45c7ca7c
Salem Owino <salemowino18@gmail.com>
2020-09-24 09:31:29 +0300

    congif added

--- mobile/settings.py
+++ mobile/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from decouple import config,Csv
+import dj_database_url
 from pathlib import Path
 import django_heroku
-import dj_database_url
-from decouple import config,Csv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
