6b260670efbe5bba54ee229617bd9391c30c7fd3
Salem Owino <salemowino18@gmail.com>
2020-09-26 13:26:24 +0300

    migrations

--- mobile/settings.py
+++ mobile/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'corsheaders.middleware.CorsPostCsrfMiddleware'
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
